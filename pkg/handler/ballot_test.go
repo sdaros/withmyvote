033837b20908d58495d24cbc0c389a3a2bac8b8d
Stefano Da Ros <sdaros@cip.li>
2018-06-17 22:52:53 +0200

    refactor: add basic tests for ballot handler

--- pkg/handler/ballot_test.go
+++ pkg/handler/ballot_test.go
@@ -1,1 +1,28 @@
 package handler
+
+import (
+	"os"
+	"testing"
+
+	"github.com/sdaros/withmyvote/pkg/app"
+	"github.com/sdaros/withmyvote/pkg/db"
+)
+
+var d db.DB
+
+func TestMain(m *testing.M) {
+	d = db.Open()
+	d.Store(db.TestData.ID, db.TestData)
+	os.Exit(m.Run())
+}
+
+// TODO
+func TestFetch(t *testing.T) {
+	val, ok := d.Load("employee-benefits")
+	if !ok {
+		t.Errorf("couldn't load issue")
+	}
+	if val.(*app.Issue).ID != "employee-benefits" {
+		t.Errorf("expected id to be employee-benefits, got %#+v", val)
+	}
+}
