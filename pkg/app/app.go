645c1e5d395dbb69393e4d0389c9de1bd33c1274
Stefano Da Ros <sdaros@cip.li>
2018-06-17 22:54:45 +0200

    refactor: add "domain specific things" to app.go

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/sdaros/withmyvote/pkg/handler"
 )
@@ -17,4 +18,18 @@ var (
 type Route struct {
 	Path    string
 	Handler http.Handler
+}
+
+// Issue is voted on
+type Issue struct {
+	ID          string
+	Description string
+	CreatedAt   time.Time
+	Candidates  []*Candidate
+}
+
+// Candidate in issue
+type Candidate struct {
+	ID   int
+	Text string
 }
